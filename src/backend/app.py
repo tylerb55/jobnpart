959d67fe9fcf218fc8a8cd1810d869a098fbac83
tylerb55 <hmtylerb@hotmail.co.uk>
2025-06-04 14:03:08 +0100

    cors policy change

--- src/backend/app.py
+++ src/backend/app.py
@@ -32,8 +32,8 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins=origins, 
-    allow_credentials=True, 
+    allow_origins=["*"], 
+    allow_credentials=False, 
     allow_methods=["*"], 
     allow_headers=["*"],
 )
